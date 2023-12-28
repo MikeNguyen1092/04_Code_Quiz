de2814088aa16a055b5ff08b4c2a1ed9cd461018
Mike Nguyen <mikenguyen@Mikes-Air.lan>
2023-12-27 23:18:21 -0500

    made answers clickable and see if they are correct.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,44 @@
 let startGame = document.querySelector("#start-button");
 let highScore = document.querySelector("#high-score");
 let questionsScript = document.querySelector("#questionsHTML");
-let answersScritp = document.querySelector("#answersHTML")
+let answerScript = document.querySelector("#answersHTML")
+let questionNumber = 0
 
 
-function init(){
 
+    // adds adds question and answer to page
+function quizGame(){
+    // clears the answers so there is no doubles.
+    answerScript.innerHTML = "";
+
+    // get the question from the question js and make it appear on the page
+    questionsScript.textContent = questions[questionNumber].q;
+
+    // get the corresponding answers and make it appear on the page as button element
+    questions[questionNumber].a.forEach(element => {
+    const listItem = document.createElement("button");
+    listItem.textContent = element;
+    answerScript.appendChild(listItem);
+
+    // Add a click event listener to each list item
+    listItem.addEventListener("click", function() {
+        // Handle the click event here
+        correctAnswer(this)
+        // You can perform any actions you want when an item is clicked
+      });
+    });
+}
+
+function correctAnswer(element) {
+    let clickedAnswer = element.textContent
+    if (clickedAnswer === questions[0].c) {
+        console.log("correct!")
+    } else {console.log("incorrect");
+    }
 }
 
 
-startGame.addEventListener("click", function(event) {
-    answersScritp.innerHTML = "";
-    questionsScript.textContent = questions[3].q;
-
-    questions[3].a.forEach(element => {
-    const listItem = document.createElement("li");
-    listItem.textContent = element;
-    answersScritp.appendChild(listItem);
+// Click button to start game - calls the function quizGame
+startGame.addEventListener("click", function() {
+quizGame();
 });
-}) 
